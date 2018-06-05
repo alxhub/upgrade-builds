2f5d66659fb0289e0e9ab20d0730383bca85b60c
Alex Rickabaugh <alx+alxhub@alxandria.net>
2018-06-05 13:56:02 -0700

    63f359ea6f ci(ivy): configure CI environments for Ivy JIT and AOT

---
+++ static.d.ts
@@ -0,0 +1,8 @@
+/**
+ * @license Angular v6.0.0-rc.5+311.sha-dc03f78.with-local-changes
+ * (c) 2010-2018 Google, Inc. https://angular.io/
+ * License: MIT
+ */
+
+  export * from './static/static';
+  
